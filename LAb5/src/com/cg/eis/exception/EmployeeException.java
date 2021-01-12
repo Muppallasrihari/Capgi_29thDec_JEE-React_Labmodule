5c7ca3f51097c029dea463441b8d0316c908247c
Muppallasrihari <vtu7331@veltechunvi.edu.in>
2021-01-12 22:36:11 +0530

    Lab Module5

---
+++ LAb5/src/com/cg/eis/exception/EmployeeException.java
@@ -0,0 +1,10 @@
+package com.cg.eis.exception;
+
+public class EmployeeException extends Exception {
+	public EmployeeException() {
+		super();
+	}
+	public EmployeeException(String errMsg) {
+		super(errMsg);
+	}
+}
